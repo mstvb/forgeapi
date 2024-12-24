3b77d8ea26ba86d904716f88173cae86472ce0d6
Manuel Staufer <manuel.staufervb@gmail.com>
2024-12-24 11:25:07 +0100

    Update achievement.py
    - change Achievment  to Dataclass
    - add documentation

--- src/game/achievement.py
+++ src/game/achievement.py
@@ -1,28 +1,64 @@
+# Python Libary Imports
+from dataclasses import dataclass
+
+# Third-Party Imports
 from fastenum import Enum
+
+# Project Package Imports
 from src.game.player import Player
 
-
 class RewardType(Enum):
+    """
+    Rewards Enum Class
+
+    EP -> 'EP' give Player Experience Points
+
+    ITEM -> 'ITEM' give Player a Item in Inventory
+    
+    """
     EP = 'EP'
     ITEM = 'ITEM'
 
 
 class RequirementType(Enum):
+    """
+    Requirements Enum Class
+
+    LOCATION -> 'LOCATION' to check if Player on a Location
+
+    ITEM -> 'ITEM' to check if Player has Item in Inventory
+    
+    """
     LOCATION = 'LOCATION'
     ITEM = 'ITEM'
 
 
+@dataclass
 class Achievement:
-    __slots__ = ['name', 'finished', 'rewards', 'requirements', 'displayed']
+    """
+    Achievment Class 
 
-    def __init__(self, name: str, subtitle: str):
-        self.name: str = name
-        self.finished: bool = False
-        self.rewards: list = []
-        self.requirements: list = []
-        self.displayed: dict = {'title': self.name.upper(), 'subtitle': subtitle}
+    Arguments:
+        name: [str] -> Name of Achievment
+        finished: [bool] -> When Achievment is finished is True else False
+        rewards: [list] -> List of Rewards
+        requirements: [list] -> List of Requirements
+        displayed: [dict] -> With Title / Subtitle
 
-    def add_rewards(self, name: str, reward_type: RewardType, data: dict):
+    Methods:
+        add_rewards: (name: str, reward_type: RewardType, data: dict) -> Add Reward to Rewards from Achievment
+        set_display: (title: str, subtitle: str) -> Set Display [Title, Subtitle] for Achievment
+        add_requirement: (name: str, requirement_type: RequirementType, data: dict) -> Add Requirement to Requirements from Achievment
+        run: (player: Player) -> Execute this Achievment with Player as Target
+        
+    """
+    name = name
+    finished = False
+    rewards = []
+    requirements = []
+    displayed = {'title': self.name.upper(), 'subtitle': subtitle}
+
+    def add_reward(self, name: str, reward_type: RewardType, data: dict):
         if data.get('item'):
             self.rewards.append(
                 {'name': name, 'type': reward_type,
@@ -33,7 +69,7 @@ class Achievement:
                  'item': data.get('ep')}
             )
         else:
-            return
+            return 0
 
     def set_display(self, title: str, subtitle: str):
         self.displayed = {'title': title.upper(), 'subtitle': subtitle}
@@ -50,7 +86,7 @@ class Achievement:
                 {'name': name, 'type': requirement_type,
                  'item': data.get('item')})
         else:
-            return
+            return 0
 
     def run(self, player: Player):
         for requirement in self.requirements:
